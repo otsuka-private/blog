c57c3e016015a923834d031225e81f61fc4dea41
大塚遥輝 <ootsukaharuki@dazhongyaohuis-MacBook-Pro.local>
2021-02-03 20:07:26 +0900

    from laptop

---
+++ assets/scripts/common_parts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+export class renderCommonParts {
+  constructor() {
+    this.renderLeftSideColumn();
+  }
+
+  renderLeftSideColumn() {
+    const leftSideColumn = document.createElement('div');
+    leftSideColumn.innerHTML = `
+    
+    `
+  }
+}
